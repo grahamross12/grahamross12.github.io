552afbddef89d74ba93584a216e3738d7d7e5bc1
Graham Ross <grahamross99@gmail.com>
2022-05-01 14:15:46 +0100

    Added recent projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,69 +4,98 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $nav = $("#nav");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: [null, "736px"],
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
-
+  // Scrolly.
+  $("#nav a, .scrolly").scrolly({
+    speed: 1000,
+    offset: function () {
+      return $nav.height();
+    },
+  });
 })(jQuery);
 
 // Get the modal
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img1 = document.getElementById("myImg1");
 var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
 
 var modalImg1 = document.getElementById("img01");
 var modalImg2 = document.getElementById("img02");
+var modalImg3 = document.getElementById("img03");
+var modalImg4 = document.getElementById("img04");
 
 var captionText1 = document.getElementById("caption1");
 var captionText2 = document.getElementById("caption2");
+var captionText3 = document.getElementById("caption3");
+var captionText4 = document.getElementById("caption4");
 
-img1.onclick = function(){
+img1.onclick = function () {
   modal1.style.display = "block";
   modalImg1.src = this.src;
   captionText1.innerHTML = this.alt;
-}
-img2.onclick = function(){
+};
+img2.onclick = function () {
   modal2.style.display = "block";
   modalImg2.src = this.src;
   captionText2.innerHTML = this.alt;
-}
+};
+img3.onclick = function () {
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+  captionText3.innerHTML = this.alt;
+};
+img4.onclick = function () {
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+  captionText4.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span1 = document.getElementsByClassName("close")[0];
-var span2 = document.getElementsByClassName("close")[1];
+var span1 = document.getElementById("close1");
+var span2 = document.getElementById("close2");
+var span3 = document.getElementById("close3");
+var span4 = document.getElementById("close4");
 
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
+span1.onclick = function () {
+  console.log(span1);
   modal1.style.display = "none";
-}
+};
 
-span2.onclick = function() {
+span2.onclick = function () {
   modal2.style.display = "none";
-}
+};
+
+span3.onclick = function () {
+  modal3.style.display = "none";
+};
+
+span4.onclick = function () {
+  modal4.style.display = "none";
+};
